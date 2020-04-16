858f0185128f4f78c59b7ce021b792a1ffca65ff
Aran Leite <hyoretsu@gmail.com>
2020-04-16 18:50:36 -0300

    Módulo 2 - Aula 1.8

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,7 @@ import routes from './routes';
 const app = express();
 
 app.use(express.json());
+
 app.use(routes);
 
-app.listen(3333, () => {
- console.log('🚀 Server started on port 3333!');
-});
+app.listen(3333);
