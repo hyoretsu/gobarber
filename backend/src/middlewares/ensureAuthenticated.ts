40ea04577c87c6c95be38c5cc8c82df981cc8ff2
Aran Leite <hyoretsu@gmail.com>
2020-04-21 14:34:31 -0300

    Módulo 2 - Aula 2.19
    Lidando melhor com erros

--- backend/src/middlewares/ensureAuthenticated.ts
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -3,6 +3,8 @@ import { verify } from 'jsonwebtoken';
 
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
 interface TokenPayload {
  iad: number;
  exp: number;
@@ -13,7 +15,7 @@ export default function ensureAuthenticated(request: Request, response: Response
  const authHeader = request.headers.authorization;
 
  if (!authHeader) {
-  throw new Error('JWT token is missing');
+  throw new AppError('JWT token is missing', 401);
  }
 
  const [, token] = authHeader.split(' ');
@@ -28,6 +30,6 @@ export default function ensureAuthenticated(request: Request, response: Response
 
   return next();
  } catch {
-  throw new Error('Invalid JWT token');
+  throw new AppError('Invalid JWT token', 401);
  }
 }
