c22778669d7cef311dc0af598a3dd9610d6e4d32
Aran Leite <hyoretsu@gmail.com>
2020-04-20 23:34:08 -0300

    Módulo 2 - Aula 2.15
    Rotas modificadas para se adaptar à autenticação JWT

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -30,9 +31,11 @@ class AuthenticateUserService {
    throw new Error('Incorrect email/password combination.');
   }
 
-  const token = sign({}, 'aa2481f29d92155e8ffdb63a9f8ff9fa', {
+  const { secret, expiresIn } = authConfig.jwt;
+
+  const token = sign({}, secret, {
    subject: user.id,
-   expiresIn: '1d',
+   expiresIn,
   });
 
   return {
