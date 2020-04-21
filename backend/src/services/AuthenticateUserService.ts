1874c063575e507d55f4f07f3ac642d0aa764af0
Aran Leite <hyoretsu@gmail.com>
2020-04-20 22:53:22 -0300

    Módulo 2 - Aula 2.14
    Autenticação atráves do JWT

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
  user: User;
+ token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,15 @@ class AuthenticateUserService {
    throw new Error('Incorrect email/password combination.');
   }
 
-  return { user };
+  const token = sign({}, 'aa2481f29d92155e8ffdb63a9f8ff9fa', {
+   subject: user.id,
+   expiresIn: '1d',
+  });
+
+  return {
+   user,
+   token,
+  };
  }
 }
 
