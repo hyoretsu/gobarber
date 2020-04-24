259fc3d9456922214987a510178215619ba6b037
Aran Leite <hyoretsu@gmail.com>
2020-04-24 17:04:23 -0300

    Módulo 3 - Aula 2.4
    Isolamento dos componentes de input e botão

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Content, Background } from './styles';
+
 const SignIn: React.FC = () => (
  <Container>
   <Content>
@@ -11,11 +15,11 @@ const SignIn: React.FC = () => (
    <form>
     <h1>Faça seu logon</h1>
 
-    <input placeholder="E-mail" />
+    <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-    <input type="password" placeholder="Senha" />
+    <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-    <button type="submit">Entrar</button>
+    <Button type="submit">Entrar</Button>
 
     <a href="forgot">Esqueci minha senha</a>
    </form>
