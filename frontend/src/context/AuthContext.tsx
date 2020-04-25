b9eb93bb9ce22a012226c199d73bdca4e37852d7
Aran Leite <hyoretsu@gmail.com>
2020-04-25 17:38:06 -0300

    Módulo 3 - Aula 2.14
    Adaptando o login ao Context e enviando credenciais à API

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useCallback } from 'react';
+import api from '../services/api';
+
+interface SignInCredentials {
+ email: string;
+ password: string;
+}
+
+interface AuthContextData {
+ name: string;
+ signIn(credentials: SignInCredentials): Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+ const signIn = useCallback(async ({ email, password }) => {
+  const response = await api.post('sessions', { email, password });
+ }, []);
+
+ return <AuthContext.Provider value={{ name: 'Diego', signIn }}>{children}</AuthContext.Provider>;
+};
+
+export { AuthContext, AuthProvider };
