daad41e373bac348ad71f7eac2ec18431fd0313d
Aran Leite <hyoretsu@gmail.com>
2020-04-26 16:36:54 -0300

    Módulo 3 - Aula 2.22
    Rotas configuradas e animação das páginas

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -4,13 +4,14 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import getValidationErrors from '../../utils/getValidationError';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 const SignUp: React.FC = () => {
  const formRef = useRef<FormHandles>(null);
@@ -38,23 +39,25 @@ const SignUp: React.FC = () => {
    <Background />
 
    <Content>
-    <img src={logoImg} alt="GoBarber"></img>
+    <AnimationContainer>
+     <img src={logoImg} alt="GoBarber"></img>
 
-    <Form ref={formRef} onSubmit={handleSubmit}>
-     <h1>Faça seu cadastro</h1>
+     <Form ref={formRef} onSubmit={handleSubmit}>
+      <h1>Faça seu cadastro</h1>
 
-     <Input name="name" icon={FiUser} placeholder="Nome" />
-     <Input name="email" icon={FiMail} placeholder="E-mail" />
+      <Input name="name" icon={FiUser} placeholder="Nome" />
+      <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-     <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+      <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-     <Button type="submit">Cadastrar</Button>
-    </Form>
+      <Button type="submit">Cadastrar</Button>
+     </Form>
 
-    <a href="login">
-     <FiArrowLeft />
-     Voltar para logon
-    </a>
+     <Link to="/">
+      <FiArrowLeft />
+      Voltar para logon
+     </Link>
+    </AnimationContainer>
    </Content>
   </Container>
  );
