4f89d3adf2afd148b1c1869b104bde1b2a136f06
Aran Leite <hyoretsu@gmail.com>
2020-04-28 20:54:18 -0300

    Módulo 3 - Aula 3.14
    Cadastro de usuários no servidor

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -5,6 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationError';
 
@@ -40,9 +41,11 @@ const SignUp: React.FC = () => {
 
    await schema.validate(data, { abortEarly: false });
 
-   //await api.post('/users', data);
+   await api.post('/users', data);
 
-   // history.push('/');
+   Alert.alert('Cadastro realizado com sucesso!', 'Você já pode fazer login na aplicação');
+
+   navigation.goBack();
   } catch (err) {
    if (err instanceof Yup.ValidationError) {
     const errors = getValidationErrors(err);
