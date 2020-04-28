c84ef9936bb85a7609c1bb79a7eb1b56d858cc7d
Aran Leite <hyoretsu@gmail.com>
2020-04-27 23:21:23 -0300

    Módulo 3 - Aula 3.9
    Tela de cadastro e navegação entre rotas

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -17,6 +18,7 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+ const navigation = useNavigation();
  return (
   <>
    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
@@ -46,7 +48,7 @@ const SignIn: React.FC = () => {
     </ScrollView>
    </KeyboardAvoidingView>
 
-   <CreateAccountButton onPress={() => {}}>
+   <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
     <Icon name="log-in" size={20} color="#ff9000"></Icon>
     <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
    </CreateAccountButton>
