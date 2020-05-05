93affb892cf66fc0f1e50c67d3a03c68952ad51d
Aran Leite <hyoretsu@gmail.com>
2020-05-04 23:44:31 -0300

    Módulo 4 - Aula 1.7
    Reescrevendo o repositório para se adaptar à separação

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -4,7 +4,7 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import AppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface Request {
  provider_id: string;
@@ -23,12 +23,10 @@ export default class CreateAppointmentService {
    throw new AppError('This appointment is already booked');
   }
 
-  const appointment = appointmentsRepository.create({
+  const appointment = await appointmentsRepository.create({
    provider_id,
    date: appointmentDate,
   });
-
-  await appointmentsRepository.save(appointment);
 
   return appointment;
  }
