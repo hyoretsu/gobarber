47d36e557a14ff64497196d427536f75d88d65b9
Aran Leite <hyoretsu@gmail.com>
2020-05-11 00:04:40 -0300

    Módulo 4 - Aula 2.3
    Recuperação de senha através de TDD

---
+++ backend/src/modules/users/infra/typeorm/entities/UserToken.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Generated } from 'typeorm';
+
+@Entity('userToken_tokens')
+export default class UserToken {
+ @PrimaryGeneratedColumn('uuid')
+ id: string;
+
+ @Column()
+ @Generated('uuid')
+ token: string;
+
+ @Column()
+ user_id: string;
+
+ @CreateDateColumn()
+ created_at: Date;
+
+ @UpdateDateColumn()
+ updated_at: Date;
+}
