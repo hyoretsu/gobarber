22eb5e847480eb1f846d8f4efe321060a2e6d47d
Aran Leite <hyoretsu@gmail.com>
2020-05-12 00:07:23 -0300

    Módulo 4 - Aula 2.4
    Reset de senha e testes básicos

--- backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -20,4 +20,10 @@ export default class FakeUserTokensRepository implements IUserTokensRepository {
 
   return userToken;
  }
+
+ public async findByToken(token: string): Promise<UserToken | undefined> {
+  const userToken = this.userTokens.find(findToken => findToken.token === token);
+
+  return userToken;
+ }
 }
