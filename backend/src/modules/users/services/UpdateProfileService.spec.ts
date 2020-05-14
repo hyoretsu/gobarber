7a4aa6b9171a5520b9f5410de2f96c92638e64ab
Aran Leite <hyoretsu@gmail.com>
2020-05-14 10:50:44 -0300

    Módulo 4 - Aula 2.12
    Rotas de listagem e modificação de dados de um usuário

--- backend/src/modules/users/services/UpdateProfileService.spec.ts
+++ backend/src/modules/users/services/UpdateProfileService.spec.ts
@@ -32,6 +32,16 @@ describe('UpdateProfile', () => {
   expect(updatedUser.email).toBe('johntre@example.com');
  });
 
+ it('should not be able update the profile of a non-existent user', async () => {
+  await expect(
+   updateProfile.execute({
+    user_id: 'non-existent-user-id',
+    name: 'Test',
+    email: 'test@example.com',
+   }),
+  ).rejects.toBeInstanceOf(AppError);
+ });
+
  it('should not be able to change to another user email', async () => {
   await fakeUsersRepository.create({
    name: 'John Doe',
