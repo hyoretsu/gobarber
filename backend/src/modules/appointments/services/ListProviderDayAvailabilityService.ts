1f343df394f12d2e4b1e929849bdce9957e9fbe6
Aran Leite <hyoretsu@gmail.com>
2020-05-14 23:15:40 -0300

    Módulo 4 - Aula 2.16
    Verificação de horários disponíveis num dia

---
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,48 @@
+import { injectable, inject } from 'tsyringe';
+import { getHours } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+ provider_id: string;
+ month: number;
+ year: number;
+ day: number;
+}
+
+type IResponse = Array<{
+ hour: number;
+ available: boolean;
+}>;
+
+@injectable()
+export default class ListProviderDayAvailabilityService {
+ constructor(
+  @inject('AppointmentsRepository')
+  private appointmentsRepository: IAppointmentsRepository,
+ ) {}
+
+ public async execute({ provider_id, month, year, day }: IRequest): Promise<IResponse> {
+  const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
+   provider_id,
+   day,
+   month,
+   year,
+  });
+
+  const hourStart = 8;
+
+  const eachHourArray = Array.from({ length: 10 }, (value, index) => index + hourStart);
+
+  const availability = eachHourArray.map(hour => {
+   const hasAppointmentInHour = appointments.find(appointment => getHours(appointment.date) === hour);
+
+   return {
+    hour,
+    available: !hasAppointmentInHour,
+   };
+  });
+
+  return availability;
+ }
+}
