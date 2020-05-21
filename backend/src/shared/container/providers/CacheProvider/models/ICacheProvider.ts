b4115dded30f9a78b4d291fca40aab7ce059e09c
Aran Leite <hyoretsu@gmail.com>
2020-05-20 21:29:34 -0300

    Módulo 4 - Aula 3.14
    Sistema de invallidação de cache

--- backend/src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ backend/src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -2,4 +2,5 @@ export default interface ICacheProvider {
  save(key: string, value: any): Promise<void>;
  recover<Type>(key: string): Promise<Type | null>;
  invalidate(key: string): Promise<void>;
+ invalidatePrefix(prefix: string): Promise<void>;
 }
