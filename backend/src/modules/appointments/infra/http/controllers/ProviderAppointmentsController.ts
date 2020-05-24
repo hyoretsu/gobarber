affd0021bdf89f57b8e8f248ef0b3f3f9d29f42e
Aran Leite <hyoretsu@gmail.com>
2020-05-24 15:10:07 -0300

    Módulo 5 - Aula 1.2
    Ajuste das rotas de listagem de agendamentos

--- backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -6,15 +6,15 @@ import ListProviderAppointmentsService from '@modules/appointments/services/List
 export default class ProviderAppointmentsController {
  public async index(req: Request, res: Response): Promise<Response> {
   const provider_id = req.user.id;
-  const { day, month, year } = req.body;
+  const { day, month, year } = req.query;
 
   const listProviderAppointments = container.resolve(ListProviderAppointmentsService);
 
   const appointments = await listProviderAppointments.execute({
    provider_id,
-   day,
-   month,
-   year,
+   day: Number(day),
+   month: Number(month),
+   year: Number(year),
   });
 
   return res.json(appointments);
