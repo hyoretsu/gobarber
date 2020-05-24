8bf799e19c8ca31ac308c6b4e53219f817297703
Aran Leite <hyoretsu@gmail.com>
2020-05-24 15:26:06 -0300

    Módulo 5 - Aula 1.4
    Verificação se dias anteriores já passaram durante a listagem de agendamentos

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -33,13 +33,15 @@ export default class ListProviderMonthAvailabilityService {
   const eachDayArray = Array.from({ length: numberOfDaysInMonth }, (value, index) => index + 1);
 
   const availability = eachDayArray.map(day => {
+   const compareDate = new Date(year, month - 1, day, 18, 0, 0);
+
    const appointmentsInDay = appointments.filter(appointment => {
     return getDate(appointment.date) === day;
    });
 
    return {
     day,
-    available: appointmentsInDay.length < 10,
+    available: isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
    };
   });
 
