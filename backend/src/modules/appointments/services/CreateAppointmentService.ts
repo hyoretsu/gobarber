ff0a1a9656608804f45f9b99b800a28c8ab14687
Aran Leite <hyoretsu@gmail.com>
2020-05-24 15:15:27 -0300

    Módulo 5 - Aula 1.3
    Verificação da existência de um agendamento em uma hora específica de um provider específico

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -42,7 +42,7 @@ export default class CreateAppointmentService {
    throw new AppError('You can only create appointments between 8AM and 5PM');
   }
 
-  const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate);
+  const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate, provider_id);
 
   if (findAppointmentInSameDate) {
    throw new AppError('This appointment is already booked');
