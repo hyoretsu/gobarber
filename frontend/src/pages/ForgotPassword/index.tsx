f2371c69fd8a0e7e06f19ba874e227375abcbac0
Aran Leite <hyoretsu@gmail.com>
2020-05-25 16:54:49 -0300

    Módulo 5 - Aula 1.11
    Envio do reset de senha à API

--- frontend/src/pages/ForgotPassword/index.tsx
+++ frontend/src/pages/ForgotPassword/index.tsx
@@ -85,7 +85,7 @@ const ForgotPassword: React.FC = () => {
       </Button>
      </Form>
 
-     <Link to="/signin">
+     <Link to="/">
       <FiLogIn />
       Voltar ao login
      </Link>
--- frontend/src/pages/ResetPassword/index.tsx
+++ frontend/src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import { FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationError';
@@ -14,6 +14,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
  password: string;
@@ -26,6 +27,7 @@ const ResetPassword: React.FC = () => {
  const { addToast } = useToast();
 
  const history = useHistory();
+ const location = useLocation();
 
  const handleSubmit = useCallback(
   async (data: ResetPasswordFormData) => {
@@ -39,7 +41,20 @@ const ResetPassword: React.FC = () => {
 
     await schema.validate(data, { abortEarly: false });
 
-    history.push('/signin');
+    const { password, password_confirmation } = data;
+    const token = location.search.replace('?token=', '');
+
+    if (!token) {
+     throw new Error();
+    }
+
+    await api.post('/password/reset', {
+     password,
+     password_confirmation,
+     token,
+    });
+
+    history.push('/');
    } catch (err) {
     if (err instanceof Yup.ValidationError) {
      const errors = getValidationErrors(err);
@@ -56,7 +71,7 @@ const ResetPassword: React.FC = () => {
     });
    }
   },
-  [addToast, history],
+  [addToast, history, location.search],
  );
  return (
   <Container>
