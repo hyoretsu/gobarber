c6247904e3188e9323a9ccf5e1d0fc00df22c199
Aran Leite <hyoretsu@gmail.com>
2020-05-26 19:18:11 -0300

    Módulo 5 - Aula 1.13
    Próximo agendamento com informações dinâmicas

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar } from './styles';
 
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
@@ -27,6 +27,33 @@ const Dashboard: React.FC = () => {
      </button>
     </HeaderContent>
    </Header>
+   <Content>
+    <Schedule>
+     <h1>Horários agendados</h1>
+     <p>
+      <span>Hoje</span>
+      <span>Dia 06</span>
+      <span>Segunda-feira</span>
+     </p>
+
+     <NextAppointment>
+      <strong>Atendimento a seguir</strong>
+      <div>
+       <img
+        src="https://avatars1.githubusercontent.com/u/20804322?s=460&u=c46a6c83796cd6fb124fbe39dddfadfff5d08388&v=4"
+        alt="Aran Leite"
+       />
+
+       <strong>Aran Leite</strong>
+       <span>
+        <FiClock />
+        08:00
+       </span>
+      </div>
+     </NextAppointment>
+    </Schedule>
+    <Calendar />
+   </Content>
   </Container>
  );
 };
