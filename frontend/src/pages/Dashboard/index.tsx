8b1a13639f6ce01f58cb1eaf339f47c389ed2c58
Aran Leite <hyoretsu@gmail.com>
2020-05-27 18:28:27 -0300

    Módulo 5 - Aula 1.18
    Listagem de agendamentos dinâmica

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react';
-import { isToday, format } from 'date-fns';
+import { isToday, format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
@@ -30,6 +30,7 @@ interface MonthAvailabilityItem {
 interface Appointment {
  id: string;
  date: string;
+ hourFormatted: string;
  user: {
   name: string;
   avatar_url: string;
@@ -71,14 +72,23 @@ const Dashboard: React.FC = () => {
 
  useEffect(() => {
   api
-   .get('/appointments/me', {
+   .get<Appointment[]>('/appointments/me', {
     params: {
      year: selectedDate.getFullYear(),
      month: selectedDate.getMonth() + 1,
      day: selectedDate.getDate(),
     },
    })
-   .then(response => setAppointments(response.data));
+   .then(response => {
+    const appointmentsFormatted = response.data.map(appointment => {
+     return {
+      ...appointment,
+      hourFormatted: format(parseISO(appointment.date), 'HH:mm'),
+     };
+    });
+
+    setAppointments(appointmentsFormatted);
+   });
  }, [selectedDate]);
 
  const disabledDays = useMemo(() => {
@@ -103,6 +113,18 @@ const Dashboard: React.FC = () => {
  const selectedWeekday = useMemo(() => {
   return format(selectedDate, 'cccc', { locale: ptBR });
  }, [selectedDate]);
+
+ const morningAppointments = useMemo(() => {
+  return appointments.filter(appointment => {
+   return parseISO(appointment.date).getHours() < 12;
+  });
+ }, [appointments]);
+
+ const afternoonAppointments = useMemo(() => {
+  return appointments.filter(appointment => {
+   return parseISO(appointment.date).getHours() >= 12;
+  });
+ }, [appointments]);
 
  return (
   <Container>
@@ -150,56 +172,38 @@ const Dashboard: React.FC = () => {
      <Section>
       <strong>Manhã</strong>
 
-      <Appointment>
-       <span>
-        <FiClock />
-        08:00
-       </span>
+      {morningAppointments.map(appointment => (
+       <Appointment key={appointment.id}>
+        <span>
+         <FiClock />
+         {appointment.hourFormatted}
+        </span>
 
-       <div>
-        <img
-         src="https://avatars1.githubusercontent.com/u/20804322?s=460&u=c46a6c83796cd6fb124fbe39dddfadfff5d08388&v=4"
-         alt="Aran Leite"
-        />
+        <div>
+         <img src={appointment.user.avatar_url} alt={appointment.user.name} />
 
-        <strong>Aran Leite</strong>
-       </div>
-      </Appointment>
-
-      <Appointment>
-       <span>
-        <FiClock />
-        08:00
-       </span>
-
-       <div>
-        <img
-         src="https://avatars1.githubusercontent.com/u/20804322?s=460&u=c46a6c83796cd6fb124fbe39dddfadfff5d08388&v=4"
-         alt="Aran Leite"
-        />
-
-        <strong>Aran Leite</strong>
-       </div>
-      </Appointment>
+         <strong>{appointment.user.name}</strong>
+        </div>
+       </Appointment>
+      ))}
      </Section>
      <Section>
       <strong>Tarde</strong>
 
-      <Appointment>
-       <span>
-        <FiClock />
-        08:00
-       </span>
+      {afternoonAppointments.map(appointment => (
+       <Appointment key={appointment.id}>
+        <span>
+         <FiClock />
+         {appointment.hourFormatted}
+        </span>
 
-       <div>
-        <img
-         src="https://avatars1.githubusercontent.com/u/20804322?s=460&u=c46a6c83796cd6fb124fbe39dddfadfff5d08388&v=4"
-         alt="Aran Leite"
-        />
+        <div>
+         <img src={appointment.user.avatar_url} alt={appointment.user.name} />
 
-        <strong>Aran Leite</strong>
-       </div>
-      </Appointment>
+         <strong>{appointment.user.name}</strong>
+        </div>
+       </Appointment>
+      ))}
      </Section>
     </Schedule>
     <Calendar>
