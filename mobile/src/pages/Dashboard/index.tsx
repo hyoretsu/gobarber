7a7f84b928d4cc5863e43f029bd2d567dc2b4a9d
Aran Leite <hyoretsu@gmail.com>
2020-06-10 18:00:46 -0300

    Módulo 5 - Aula 2.6
    Busca dos providers da API

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,17 +1,32 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar } from './styles';
+import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar, ProvidersList } from './styles';
+import api from '../../services/api';
+
+export interface Provider {
+ id: string;
+ name: string;
+ avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
+ const [providers, setProviders] = useState<Provider[]>([]);
+
  const { signOut, user } = useAuth();
  const { navigate } = useNavigation();
 
  const navigateToProfile = useCallback(() => {
-  navigate('Profile');
- }, [navigate]);
+  signOut();
+ }, [signOut]);
+
+ useEffect(() => {
+  api.get('providers').then(response => {
+   setProviders(response.data);
+  });
+ }, []);
 
  return (
   <Container>
@@ -25,6 +40,12 @@ const Dashboard: React.FC = () => {
      <UserAvatar source={{ uri: user.avatar_url }} />
     </ProfileButton>
    </Header>
+
+   <ProvidersList
+    data={providers}
+    keyExtractor={provider => provider.id}
+    renderItem={({ item }) => <UserName>{item.name}</UserName>}
+   />
   </Container>
  );
 };
