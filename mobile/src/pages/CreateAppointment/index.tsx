e0cc246d563a30dc5c73c1d59035118c3992896c
Aran Leite <hyoretsu@gmail.com>
2020-06-16 20:36:44 -0300

    Módulo 5 - Aula 2.13
    Disponibilidade de manhã e tarde

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState, useMemo } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import { Platform } from 'react-native';
+import { format } from 'date-fns';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 import { useAuth } from '../../hooks/auth';
@@ -45,7 +46,7 @@ const CreateAppointment: React.FC = () => {
 
  const routeParams = route.params as RouteParams;
 
- const [availability, setaAvailability] = useState<AvailabilityItem[]>();
+ const [availability, setaAvailability] = useState<AvailabilityItem[]>([]);
  const [showDatePicker, setShowDatePicker] = useState(false);
  const [selectedDate, setSelectedDate] = useState(new Date());
  const [providers, setProviders] = useState<Provider[]>([]);
@@ -90,6 +91,30 @@ const CreateAppointment: React.FC = () => {
    setSelectedDate(date);
   }
  }, []);
+
+ const morningAvailability = useMemo(() => {
+  return availability
+   .filter(({ hour }) => hour < 12)
+   .map(({ hour, available }) => {
+    return {
+     hour,
+     available,
+     hourFormatted: format(new Date().setHours(hour), 'HH:00'),
+    };
+   });
+ }, [availability]);
+
+ const afternoonAvailability = useMemo(() => {
+  return availability
+   .filter(({ hour }) => hour >= 12)
+   .map(({ hour, available }) => {
+    return {
+     hour,
+     available,
+     hourFormatted: format(new Date().setHours(hour), 'HH:00'),
+    };
+   });
+ }, [availability]);
 
  return (
   <Container>
@@ -137,6 +162,14 @@ const CreateAppointment: React.FC = () => {
      />
     )}
    </Calendar>
+
+   {morningAvailability.map(({ hourFormatted, available }) => (
+    <Title key={hourFormatted}>{hourFormatted}</Title>
+   ))}
+
+   {afternoonAvailability.map(({ hourFormatted, available }) => (
+    <Title key={hourFormatted}>{hourFormatted}</Title>
+   ))}
   </Container>
  );
 };
--- mobile/src/pages/CreateAppointment/styles.ts
+++ mobile/src/pages/CreateAppointment/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import { Platform, FlatList } from 'react-native';
+import { FlatList } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { RectButton } from 'react-native-gesture-handler';
 import { Provider } from './index';
